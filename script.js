d09c5514b569bf1550093fcb4f7fef0d409036c5
Aisha <fatimaaisha002@gmail.com>
2024-06-24 15:55:25 +0500

    NEW FILE

---
+++ script.js
@@ -0,0 +1,51 @@
+// let cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toLowerCase();
+// let cleanestCities = ["cheyenne", "santa fe", "tucson", "great falls", "honolulu"]; for (let i = 0; 
+// i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) { alert("It's one of the cleanest cities");
+
+// }
+// }
+//indexof,lastindexof method
+// let country="Pakistan";
+// console.log(country.lastIndexOf("a"));
+
+// let text="To be or to not be";
+// let segIndex=text.lastIndexOf("be");
+// console.log(segIndex)
+//charAT METHOD
+// let firstNmae=prompt("enter your first  name")
+// let firstChar=firstNmae.charAt(1,2);
+// console.log(firstChar)
+
+
+//
+// let text="hello world";
+// let letter=text.charAt(text.length-4)
+// console.log(letter)
+
+
+//replace method
+// let text ="visit microsoft"
+// let  result=text.replace("microsoft","chatgpt")
+// console.log(result)
+
+//
+// let text="Mr Blue has a blue house and blue car";
+// let result=text.replace(/blue/gi,"red")
+// console.log(result)
+
+//math.round method
+// let scoreAvg=Math.round(4.78);
+// console.log(scoreAvg)
+
+//CEIL FLOOR METHOD
+// let ceilNUM=Math.ceil(3.501)
+// console.log(ceilNUM)
+// // let floorNum=Math.floor(4.001)
+// // console.log(floorNum)
+
+//random number method
+
+// let num=Math.floor(Math.random()*4)
+// console.log(num)
