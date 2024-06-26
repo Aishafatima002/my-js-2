7666a1b26406d34deb88fa0d734d26f377afd4c0
Aisha <fatimaaisha002@gmail.com>
2024-06-26 16:04:42 +0500

    up

---
+++ app.js
@@ -0,0 +1,61 @@
+// chap 31 getting current date ntime
+// cons =new
+// let rightnow=new Date()
+// document.write(rightnow)
+ 
+// var rightnow1=rightnow.toString()
+// document.write("<br>"+rightnow1)
+
+// let rightnow=new Date()
+// let day=rightnow.getMonth()
+// document.write(day)
+
+//
+// var dayName=["sun","mon","tue","wed","thurs","fri","sat"]
+// var now = new Date();
+//  var theDay = now.getDay();
+//  var nameOfToday = dayName[theDay];
+//  console.log(nameOfToday)
+
+// var dayName=["jan","feb","march","april","may","june","july"]
+// var now = new Date();
+//  var theDay = now.getMonth();
+//  var nameOfToday = dayName[theDay];
+//  console.log(nameOfToday)
+
+// //
+// var d = new Date();
+//  var currentMonth = d.getMonth();
+//  console.log(currentMonth)
+
+//  var d = new Date();
+//  var currentdate = d.getDate();
+//  console.log(currentdate)
+
+//  //
+//  var d = new Date();
+//  var currentyr = d.getFullYear();
+//  console.log(currentyr   )
+
+//  //
+//  var d = new Date();
+//  var currenthr = d.getHours();
+//  console.log(currenthr)
+
+//  //
+//  var d = new Date();
+//  var currentMin = d.getMinutes();
+//  console.log(currentMin)
+
+//  //
+//  var d = new Date();
+//  var currentsec = d.getSeconds();
+//  console.log(currentsec)
+
+// //chap 33
+// var d = new Date();
+// var d1=new Date("21 june,2005")
+// console.log(d1)
+
+// // get time
+//in millisecond
--- script.js
+++ script.js
@@ -49,3 +49,68 @@
 
 // let num=Math.floor(Math.random()*4)
 // console.log(num)
+
+// converting strings to integers and numbers!
+// let num=prompt("enter your age");
+// let age=num-18
+// console.log(age)
+
+// let profit="2500"
+// let total ="2500"-1800
+// console.log(total)
+
+// let nmu="duck"
+// let num2=24-nmu
+// console.log(num2)
+// alert("tihs is not a number",num2)
+
+
+//+is used for cocate so we use parseint with bracket()+?
+// let num=prompt("enter your age");
+// let age= parseInt(num+18)
+// console.log(age)
+
+//make the value 1 not round off
+// let myInteger=parseInt(1.999)
+// console.log(myInteger)
+
+//make the value as it is same
+// let myInteger=parseFloat(1.999)
+// console.log(myInteger)
+
+//
+//  let num=+prompt("enter your age");
+//  let age= num+18
+//  console.log(age)
+
+//CHAP29:CONVERT STRINGS TO NUMN NUM TO STRINGS!
+
+// number method!convert string to numbers
+// let myString="245"
+// let num=Number(myString)
+// console.log(num)
+// let floatingNumString = "24.9876";
+//  let num = Number(floatingNumString);
+//  console.log(num)
+
+ //convert num to string! .myString()method!
+//  let numberAsNumber = 1234;
+//  let numberAsString = numberAsNumber.toString();
+//  console.log(numberAsString)
+
+// // to fixed method! chap 30
+// let price=9.95
+// let sale=0.065
+// let total=price+(price * sale)
+// let prettyTotal=total.toFixed(2)
+// console.log(prettyTotal)
+
+// let num=12345
+// var str = num.toString();
+// if (str.charAt(str.length ) === "5") {
+// str = str.slice(0,str.length  - 1) + "6";//replace the last char with string
+// }
+// num = Number(str);
+// prettyNum = num.toFixed(2);
+// console.log(prettyNum)
+
